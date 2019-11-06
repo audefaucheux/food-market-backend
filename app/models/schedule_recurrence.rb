1171e680227dd8c46399a98909790e4b9404f65a
github username <aude53@gmail.com>
2019-11-06 16:49:50 +0000

    validates if the food truck id and day num are unique in schedule recurrence

--- app/models/schedule_recurrence.rb
+++ app/models/schedule_recurrence.rb
@@ -1,16 +1,8 @@
 class ScheduleRecurrence < ApplicationRecord
   belongs_to :market
   belongs_to :food_truck
-  # validates :day_num, uniqueness: true
+  validates :day_num, uniqueness: {scope: :food_truck_id}
   validates :day_num, presence: true
   validates :from_time, presence: true
   validates :to_time, presence: true
-  # validate :unique_week_day_per_truck
-
-  # def unique_week_day_per_truck
-  #   if day_num === 1
-  #     errors.add(:day_num, "can't be Monday")
-  #   end
-  # end
-
 end
